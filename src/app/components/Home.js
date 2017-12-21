6dcd90410563a0ed8c48556e8ee029121214d57d
Dmitriy_Sankevich <Dmitriy_Sankevich@epam.com>
2017-12-21 18:57:07 +0600

    ReactJS Basics - #7 Events & ReactJS

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,23 +2,27 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 export class Home extends React.Component{
-    render(){
 
-        console.log(this.props);
-        let text = "Something!";
+    constructor(props){
+        super(props);
+        this.age = props.age;
+
+        this.onMakeOlder = this.onMakeOlder.bind(this);
+    }
+
+    onMakeOlder(){
+        this.age++;
+        console.log(this.age);
+    }
+
+    render(){
         return(
             <div>
                 <p>In a new Component!</p>
-                <p>{text}</p>
-                <p>Your name is {this.props.name}, and your age is {this.props.age} years.</p>
-                <p>User Object => Name: {this.props.user.name}</p>
-                <div>
-                    <h4>Hobbies</h4>
-                    <ul>
-                        {this.props.user.hobbies.map((hobby, i)=><li key={i}>{hobby}</li>)}
-                    </ul>
-                </div>
-                {this.props.children}
+                <p>Your name is {this.props.name}, and your age is {this.age} years.</p>
+                <hr/>
+                <button className="btn btn-primary" onClick={this.onMakeOlder}>Make me older!</button>
+                <button className="btn btn-primary" onClick={()=>this.onMakeOlder()}>Make me older!</button>
             </div>
         );
     }
@@ -27,6 +31,4 @@ export class Home extends React.Component{
 Home.propTypes = {
     name: PropTypes.string,
     age: PropTypes.number,
-    user: PropTypes.object,
-    children: PropTypes.element.isRequired
 };
--- src/app/index.js
+++ src/app/index.js
@@ -6,12 +6,6 @@ import { Home } from ".\\components\\Home";
 
 class App extends React.Component{
     render(){
-        let user = {
-            name:"Anna",
-            hobbies:["Sports", "Reading"]
-        };
-
-
         return(
         <div className="container">
             <div className="row">
@@ -21,10 +15,7 @@ class App extends React.Component{
             </div>
             <div className="row">
                 <div className="cal-xs-10 col-xs-offset-1">
-                    <Home name="Dmitriy" age={33} user={user}>
-                        <p>This is a paragraph!</p>
-                    </Home>
-                    {/* <Home/> */}
+                    <Home name="Dmitriy" age={33}/>
                 </div>
             </div>
         </div>
