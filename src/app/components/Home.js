f044e603f4ece787b4137817b9e6ac0a59d728d5
Dmitriy_Sankevich <Dmitriy_Sankevich@epam.com>
2017-12-26 18:28:14 +0600

    ReactJS Basics - #15 React Router - Route Setup

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,103 +1,11 @@
 import React from "react";
-import PropTypes from 'prop-types';
 
 export class Home extends React.Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-            age: props.initialAge,
-            status: 0,
-            homeLink: props.initialLinkName
-        };
-//------------------------------------------------
-        setTimeout(
-            ()=>this.setState({status:1})
-            ,3000);
-//------------------------------------------------
-            this.onMakeOlder = this.onMakeOlder.bind(this);
-            this.onChangeLinkName = this.onChangeLinkName.bind(this);
-            this.onHandleChange = this.onHandleChange.bind(this);
-//------------------------------------------------
-            console.log("Constructor");
-    }
-
-    componentWillMount(){
-        console.log("componentWillMount");
-    }
-
-    componentDidMount(){
-        console.log("componentDidMount");
-    }
-
-    componentWillReceiveProps(nextProps){
-        console.log("componentWillReciveProps", nextProps);
-    }
-
-    shouldComponentUpdate(nextProps, nextState){
-        console.log("shouldComponentUpdate", nextProps, nextState);
-        // if(nextState.status === 1){
-        //     return false;
-        // }
-        return true;
-    }
-
-    componentWillUpdate(nextProps, nextState){
-        console.log("componentWillUpdate", nextProps,  nextState);
-    }
-
-    componentDidUpdate(prevProps, prevState){
-        console.log("componentDidUpdate", prevProps, prevState);
-    }
-
-    componentWillUnmount(){
-        console.log("componentWillUnmount");
-    }
-
-
-
-    onMakeOlder(){
-        this.setState(
-            {
-                age: ++this.state.age
-            }
-        );
-        console.log(this.age);
-    }
-
-    onChangeLinkName(){
-        this.props.changeLink(this.state.homeLink);
-    }
-
-    onHandleChange(event){
-        this.setState(
-        {
-            homeLink:event.target.value
-        }
-    );
-    }
-
     render(){
         return(
-            <div>
-                <p>In a new Component!</p>
-                <p>Your name is {this.props.name}, and your age is {this.state.age} years.</p>
-                <p>Status: {this.state.status}</p>
-                <hr/>
-                <button className="btn btn-primary" onClick={this.onMakeOlder}>Make me older!</button>
-                <hr/>
-                <button className="btn btn-primary" onClick={this.props.greet}>Greet</button>
-                <hr/>
-                <input type="text" value={this.state.homeLink} onChange={this.onHandleChange}/>
-                <button className="btn btn-primary" onClick={this.onChangeLinkName}>Change header</button>                
-            </div>
+        <div>
+            <h3>Home</h3>
+        </div>
         );
     }
 }
-
-Home.propTypes = {
-    name: PropTypes.string,
-    initialAge: PropTypes.number,
-    greet: PropTypes.func,
-    initialLinkName: PropTypes.string
-};
--- src/app/index.js
+++ src/app/index.js
@@ -1,79 +1,208 @@
-import React from 'react';
-import { render } from 'react-dom';
+// https://css-tricks.com/react-router-4/
+import React from "react";
+import { render } from "react-dom";
 
-import { Header } from ".\\components\\Header";
-import { Home } from ".\\components\\Home";
+import { BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 
-class App extends React.Component{
+// const PrimaryLayout = () => (
+//     <div className="primary-layout">
+//       <header>
+//         Our React Router 4 App
+//       </header>
+//       <main>
+//         <Route path="/" exact component={HomePage} />
+//         <Route path="/users" component={UsersPage} />
+//       </main>
+//     </div>
+//   )
+  
+// const PrimaryLayout = () => (
+//     <div className="primary-layout">
+//       <header>
+//         Our React Router 4 App
+//         <Route path="/users" component={UsersMenu} />
+//       </header>
+//       ------------------------------------------------------
+//       <main>
+//         <Route path="/" exact component={HomePage} />
+//         <Route path="/users" component={UsersPage} />
+//       </main>
+//     </div>
+//   )
 
-    constructor(props){
-        super(props);
-        this.state = {
-            homeLink: "Home",
-            homeMounted: true
-        };
+// const PrimaryLayout = () => (
+//     <div className="primary-layout">
+//       {/* <PrimaryHeader /> */}
+//       <main>
+//         <Switch>
+//           <Route path="/" exact component={HomePage} />
+//           <Route path="/users/add" component={UserAddPage} />
+//           <Route path="/users" component={UsersPage} />
+//           <Redirect to="/" />
+//         </Switch>
+//       </main>
+//     </div>
+//   )
 
-        this.onChangeLinkName = this.onChangeLinkName.bind(this);
-        this.onChangeHomeMounted = this.onChangeHomeMounted.bind(this);
-    }
 
-    onGreet(){
-        alert("Hello");
-    }
+// ///Bad approach to nested routing
+// const PrimaryLayout = props => {
+//     return (
+//       <div className="primary-layout">
+//         {/* <PrimaryHeader /> */}
+//         <main>
+//           <Switch>
+//             <Route path="/" exact component={HomePage} />
+//             <Route path="/users" exact component={BrowseUsersPage} />
+//             <Route path="/users/:userId" component={UserProfilePage} />
+//             <Route path="/products" exact component={BrowseProductsPage} />
+//             <Route path="/products/:productId" component={ProductProfilePage} />
+//             <Redirect to="/" />
+//           </Switch>
+//         </main>
+//       </div>
+//     )
+//   };
 
-    onChangeLinkName(newName){
-        this.setState(
-            {
-                homeLink: newName
-            }
-        );
-    }
+///Good approach to nested routing
+const PrimaryLayout = props => {
+    return (
+      <div className="primary-layout">
+        {/* <PrimaryHeader /> */}
+        <main>
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/users" component={UserSubLayout} />
+            <Route path="/products" component={ProductSubLayout} />
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </div>
+    )
+  };
 
-    onChangeHomeMounted(){
-        this.setState(
-            {
-                homeMounted: !this.state.homeMounted
-            }
-        );
-    }
 
-    render(){
-        let homeCmp = "";
 
-        if(this.state.homeMounted){
-            homeCmp = 
-                (<Home 
-                    name="Dmitriy" 
-                    initialAge={33} 
-                    greet={this.onGreet}
-                    changeLink={this.onChangeLinkName}
-                    initialLinkName={this.state.homeLink}
-                />);
-        }
+  const HomePage =() => <div>Home Page</div>;
+  const UsersPage = () => <div>Users Page</div>;
+  const UsersMenu = ()=><div>Users Menu</div>;
+  const UserAddPage = ()=><div>User Add Page</div>;
+  const BrowseUsersPage = ()=><div>Browse Users Page</div>;
+  const UserProfilePage = ()=><div>User Profile Page</div>;
+  const BrowseProductsPage = ()=><div>Browse Products Page</div>;
+  const ProductProfilePage = ()=><div>Product Profile Page</div>;
 
-        return(
-            <div className="container">
-                <div className="row">
-                    <div className="cal-xs-10 col-xs-offset-1">
-                        <Header homeLink={this.state.homeLink}/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="cal-xs-10 col-xs-offset-1">
-                        {homeCmp}
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="cal-xs-10 col-xs-offset-1">
-                        <button className="btn btn-primary" onClick={this.onChangeHomeMounted}>(Un)Mount Home Component</button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+//   const UserSubLayout = () => (
+//     <div className="user-sub-layout">
+//       <aside>
+//         {/* <UserNav /> */}
+//       </aside>
+//       <div className="primary-content">
+//         <Switch>
+//           <Route path="/users" exact component={BrowseUsersPage} />
+//           <Route path="/users/:userId" component={UserProfilePage} />
+//         </Switch>
+//       </div>
+//     </div>
+//   )
+
+  const UserSubLayout = props => (
+    <div className="user-sub-layout">
+      <aside>
+        {/* <UserNav /> */}
+      </aside>
+      <div className="primary-content">
+        <Switch>
+          <Route path={props.match.path} exact component={BrowseUsersPage} />
+          <Route path={`${props.match.path}/:userId`} component={UserProfilePage} />
+        </Switch>
+      </div>
+    </div>
+  )
+
+
+  const ProductSubLayout = ()=><div>Product Sub Layout</div>;
+  
+  const App = () => (
+    <BrowserRouter>
+      <PrimaryLayout />
+    </BrowserRouter>
+  )
+
 
 render(<App/>, window.document.getElementById("app"));
+
+
+
+// import React from "react";
+// import { render } from "react-dom";
+// // import {Router, Route, browserHistory} from "react-router";
+
+// import { BrowserRouter as Router, Route, browserHistory ,Switch} from "react-router-dom";
+
+// import { Root } from ".\\components\\Root";
+// import { Home } from ".\\components\\Home";
+// import { User } from ".\\components\\User";
+
+// // class App extends React.Component{
+// //     render(){
+// //         return(
+// //             <Router history={browserHistory}>
+// //                 <Root>
+// //                     <Route path='/user' component={User} />
+// //                     <Route path='/home' component={Home} />
+// //                 </Root>
+// //             </Router>
+// //         );
+// //     }
+// // }
+
+// class App extends React.Component{
+//     render(){
+//         return(
+//             <Router history={browserHistory}>
+//                 <div>
+//                     <Route path='/'>
+//                         <Switch>
+//                             <Route path='user' component={User} />
+//                             <Route path='home' component={Home} />
+//                         </Switch>
+//                     </Route>
+//                 </div>
+//             </Router>
+//         );
+//     }
+// }
+
+// render(<App/>, window.document.getElementById("app"));
+
+
+
+// ----------------------------------------------------------------------------------------
+
+// import React from "react";
+// import { render } from "react-dom";
+// import {Router, Route, browserHistory} from "react-router";
+
+// import { Root } from ".\\components\\Root";
+// import { Home } from ".\\components\\Home";
+// import { User } from ".\\components\\User";
+
+// class App extends React.Component{
+//     render(){
+//         return(
+//             <Router history={browserHistory}>
+//                 <Route path={"user"} component={User}/>
+//                 <Route path={"home"} component={Home}/>
+//             </Router>
+//             // <Root>
+//             //     <Home></Home>
+//             // </Root>
+//         );
+//     }
+// }
+
+// render(<App/>, window.document.getElementById("app"));
 
 
 
